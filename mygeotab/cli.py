4b626ecbc2c81126348feb04e5b2577ad5335d4f
Aaron Toth <atoth89@gmail.com>
2014-05-13 00:11:45 -0400

    Start of credentials serialization

--- mygeotab/cli.py
+++ mygeotab/cli.py
@@ -1,16 +1,29 @@
 import code
 import sys
+import os.path
+import pickle
 
 import click
 
 from mygeotab import __version__
 from mygeotab.api import *
 
+@click.command()
+def forget():
+    print 'forgotten'
+
 
 @click.command()
-def main():
+@click.pass_context
+def main(ctx):
+    try:
+        credentials = pickle.load(open(os.path.join(os.path.expanduser('~'), '.mygeotabcredentials')))
+    except IOError:
+        credentials = None
+
     code.interact(
-        'MyGeotab Python Library {0}\nPython {1} on {2}'.format(__version__, sys.version.replace('\n', ''), sys.platform),
+        'MyGeotab Python Library {0}\nPython {1} on {2}'.format(__version__, sys.version.replace('\n', ''),
+                                                                sys.platform),
         local=globals())
 
 
