09aadb897125fe1df4908e0a6e245adc4607394f
Aaron Toth <atoth89@gmail.com>
2015-01-11 21:10:19 -0500

    Added additional info for PyPI submission

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import mygeotab
 
 from setuptools import setup, find_packages
 
 setup(
     name='mygeotab',
     author='Aaron Toth',
-    version='0.1.1',
-    url='https://my.geotab.com/sdk',
+    version=mygeotab.__version__,
+    url='https://github.com/aaront/mygeotab',
+    description='An unofficial Python client for the MyGeotab API.',
     long_description=open('README.rst').read(),
     install_requires=[
         'requests',
@@ -19,8 +21,23 @@ setup(
     test_suite="tests",
     include_package_data=True,
     packages=find_packages(),
+    package_data={'': ['LICENSE', 'NOTICE']},
+    package_dir={'mygeotab': 'mygeotab'},
+    license='Apache 2.0',
     entry_points='''
         [console_scripts]
         mygeotab=mygeotab.cli:main
     ''',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries'
+    ),
 )
