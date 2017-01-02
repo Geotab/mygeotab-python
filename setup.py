2a5debd4f9f08eb866e5c3ea060dc8ab1ccba961
Aaron Toth <atoth89@gmail.com>
2017-01-02 16:31:10 -0500

    Switch to CircleCI

--- setup.py
+++ setup.py
@@ -11,10 +11,14 @@ with open('mygeotab/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-with open('README.rst', 'r') as f:
-    readme = f.read()
-with open('CHANGELOG.rst', 'r') as f:
-    changelog = f.read()
+try:
+    with open('README.rst', 'r') as f:
+        readme = f.read()
+    with open('CHANGELOG.rst', 'r') as f:
+        changelog = f.read()
+except IOError:
+    readme = ''
+    changelog = ''
 
 setup(
     name='mygeotab',
