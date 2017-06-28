01454303872ea7e4044043fb3da2f6a77953c9d5
Aaron Toth <atoth89@gmail.com>
2017-06-28 00:26:33 -0400

    Quick docs experiment with dates module.

--- mygeotab/dates.py
+++ mygeotab/dates.py
@@ -16,7 +16,9 @@ def format_iso_datetime(datetime_obj):
     """Formats the given datetime as a UTC-zoned ISO 8601 date string.
 
     :param datetime_obj: The datetime object.
+    :type datetime_obj: datetime
     :return: The datetime object in 8601 string form.
+    :rtype: datetime
     """
     datetime_obj = localize_datetime(datetime_obj, pytz.utc)
     return datetime_obj.replace(tzinfo=None).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
@@ -25,10 +27,12 @@ def format_iso_datetime(datetime_obj):
 def localize_datetime(datetime_obj, tz=pytz.utc):
     """Converts a naive or UTC-localized date into the provided timezone.
 
-    :rtype: datetime
     :param datetime_obj: The datetime object.
+    :type datetime_obj: datetime
     :param tz: The timezone. If blank or None, UTC is used.
+    :type tz: datetime.tzinfo
     :return: The localized datetime object.
+    :rtype: datetime
     """
     if not datetime_obj.tzinfo:
         return tz.localize(datetime_obj)
