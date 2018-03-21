3891bc33ab2de8fa155f5112154d092d1aac424d
Aaron Toth <atoth89@gmail.com>
2018-03-21 12:44:02 -0400

    Since all MyGeotab servers use TLS 1.2, we must force a version of Python 2 that supports this (2.7.9+).
    Addresses found issue in #91.

--- setup.py
+++ setup.py
@@ -21,11 +21,13 @@ except IOError:
     readme = ''
     changelog = ''
 
-py_version = sys.version_info[:2]
+py_version = sys.version_info[:3]
+
+if py_version < (2, 7, 9):
+    raise RuntimeError("This package requres Python 2.7.9+")
 
 packages = ['mygeotab', 'mygeotab/ext']
-
-if py_version >= (3, 5):
+if py_version >= (3, 5, 0):
     packages.append('mygeotab/async')
 
 setup(
