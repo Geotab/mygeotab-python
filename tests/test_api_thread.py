1990b676715c1a78ddd22e77ddccd1cf3825ef8b
Aaron Toth <atoth89@gmail.com>
2019-06-07 14:20:52 -0400

    Added test to ensure mygeotab can be imported in a thread without an asyncio loop in Py3.5+

---
+++ tests/test_api_thread.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import threading
+
+
+class TestAsyncThreadImport:
+    def test_thread_import(self):
+        def thread_func(results):
+            try:
+                import mygeotab
+                mygeotab.API('testuser', session_id='abc123')
+                results.append(None)
+            except Exception as e:
+                results.append(e)
+        results = []
+        thread = threading.Thread(target=thread_func, args=(results, ))
+        thread.start()
+        thread.join()
+        assert len(results) == 1
+        assert results[0] is None
