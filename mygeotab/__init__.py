e46800a2a072f6703380fcbebdb4fb2f7981b599
Aaron Toth <atoth89@gmail.com>
2021-02-05 21:29:23 -0500

    Updated versioning Switched setup.py to setup.cfg and modern Python packaging

--- mygeotab/__init__.py
+++ mygeotab/__init__.py
@@ -2,10 +2,21 @@
 
 __title__ = "mygeotab-python"
 __author__ = "Aaron Toth"
-__version__ = "0.8.6"
 
-from .api import API, AsyncAPI, server_call, server_call_async, Credentials
-from .exceptions import MyGeotabException, AuthenticationException, TimeoutException
+from importlib_metadata import PackageNotFoundError, version
+
+try:
+    VERSION = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    try:
+        from .version import version as VERSION  # noqa
+    except ImportError:  # pragma: no cover
+        raise ImportError("Failed to get the version")
+__version__ = VERSION
+
+from .api import API, AsyncAPI, Credentials, server_call, server_call_async
+from .exceptions import (AuthenticationException, MyGeotabException,
+                         TimeoutException)
 
 __all__ = [
     "API",
--- setup.py
+++ setup.py
@@ -1,58 +1,6 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 
-import ast
-import re
+import setuptools
 
-from setuptools import setup
-
-
-_version_re = re.compile(r"__version__\s+=\s+(.*)")
-with open("mygeotab/__init__.py", "rb") as f:
-    version = str(ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1)))
-
-try:
-    with open("README.rst", "r") as f:
-        readme = f.read()
-    with open("CHANGELOG.rst", "r") as f:
-        changelog = f.read()
-except IOError:
-    readme = ""
-    changelog = ""
-
-packages = ["mygeotab", "mygeotab/ext"]
-
-setup(
-    name="mygeotab",
-    author="Aaron Toth",
-    version=version,
-    url="https://github.com/geotab/mygeotab-python",
-    description="An unofficial Python client for the MyGeotab API",
-    long_description=f"{readme}\n\n{changelog}",
-    extras_require={"notebook": ["pandas"]},
-    test_suite="tests",
-    include_package_data=True,
-    packages=packages,
-    package_data={"": ["LICENSE"]},
-    license="Apache 2.0",
-    install_requires=["httpx[http2,brotli]", "click", "pytz", "arrow", "python-rapidjson"],
-    setup_requires=["wheel"],
-    python_requires=">=3.6",
-    entry_points="""
-        [console_scripts]
-        myg=mygeotab.cli:main
-    """,
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Software Development :: Libraries",
-    ],
-)
+if __name__ == "__main__":
+    setuptools.setup(use_scm_version=True)
