424c908b872ba941946cfeb5d263c0fbf60cb6f8
Aaron Toth <atoth89@gmail.com>
2021-02-14 15:56:36 -0500

    Removed # -*- coding: utf-8 -*- from source files

--- mygeotab/__init__.py
+++ mygeotab/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 __title__ = "mygeotab-python"
 __author__ = "Aaron Toth"
 __version__ = "0.0.0"
--- mygeotab/ext/entitylist.py
+++ mygeotab/ext/entitylist.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from collections import UserList
 
 from mygeotab import api
